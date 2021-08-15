61f22852033f403c656dfda765db8d52354e4dc2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-15 02:57:34 +0200

    vayu: Update blobs to V12.5.3.0.RJUMIXM
    Change-Id: I221f896aca3d3748d0a07bace9f45dc1613f7474

--- libinit/init_vayu.cpp
+++ libinit/init_vayu.cpp
@@ -6,8 +6,8 @@
 
 #include <libinit_msmnile.h>
 
-#define DESCRIPTION "vayu_global-user 11 RKQ1.200826.002 V12.5.2.0.RJUMIXM release-keys"
-#define FINGERPRINT "POCO/vayu_global/vayu:11/RKQ1.200826.002/V12.5.2.0.RJUMIXM:user/release-keys"
+#define DESCRIPTION "vayu_global-user 11 RKQ1.200826.002 V12.5.3.0.RJUMIXM release-keys"
+#define FINGERPRINT "POCO/vayu_global/vayu:11/RKQ1.200826.002/V12.5.3.0.RJUMIXM:user/release-keys"
 
 static const variant_info_t vayu_global_info = {
     .hwc_value = "GLOBAL",
