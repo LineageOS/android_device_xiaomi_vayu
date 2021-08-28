24fadadd2de3ff297f2df344cbb7f7325fec91e9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-28 18:22:36 +0200

    vayu: overlay-lineage: Define fingerprint sensor location
    Change-Id: If848dffbb42065d1fe04f33a1fb634b9af9bc273

--- device.mk
+++ device.mk
@@ -11,7 +11,8 @@ $(call inherit-product, device/xiaomi/sm8150-common/msmnile.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
