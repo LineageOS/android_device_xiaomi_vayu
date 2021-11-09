d367ceacb5ab1f4273d61004853c739878233bdf
lu5ck <skylu5ck@gmail.com>
2021-11-09 17:18:29 +0800

    Device Vayu: Remove megvii dependecy in camera.qcom.so
    Based off I97058c320d06819a468ebe38e2704360e9aa91db
    
    Change-Id: If18dd57c6f68f05b050d4131053bb68a97d550ca

--- extract-files.sh
+++ extract-files.sh
@@ -18,4 +18,14 @@ export DEVICE=vayu
 export DEVICE_COMMON=sm8150-common
 export VENDOR=xiaomi
 
+function blob_fixup() {
+    case "${1}" in
+    vendor/lib64/hw/camera.qcom.so)
+        patchelf --remove-needed "libmegface.so" "${2}"
+        patchelf --remove-needed "libMegviiFacepp-0.5.2.so" "${2}"
+        patchelf --add-needed "libshim_megvii.so" "${2}"
+        ;;
+    esac
+}
+
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
