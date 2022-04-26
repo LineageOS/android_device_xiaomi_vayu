26d82c7faf29231cc2297bbff46438e8ae462292
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-27 00:12:11 +0200

    vayu: Update blobs from V12.5.9.0.RJUMIXM
    Change-Id: I31a9f39abc88ab275dd328ab8a1d9872975023e9

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -23,6 +23,6 @@ PRODUCT_MODEL := POCO X3 Pro
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vayu_global-user 11 RKQ1.200826.002 V12.5.7.0.RJUMIXM release-keys"
+    PRIVATE_BUILD_DESC="vayu_global-user 11 RKQ1.200826.002 V12.5.9.0.RJUMIXM release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:11/RKQ1.200826.002/V12.5.7.0.RJUMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:11/RKQ1.200826.002/V12.5.9.0.RJUMIXM:user/release-keys
