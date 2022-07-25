ee5d9067d054fce4fd41eeee8e8fc1ba73a8a6ab
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-25 20:53:58 +0200

    vayu: libinit: Simplify variants split
    Change-Id: I589b6e37558693fe6b5b8291b8e92aee4609d909

--- libinit/init_xiaomi_vayu.cpp
+++ libinit/init_xiaomi_vayu.cpp
@@ -11,32 +11,6 @@
 
 #define FINGERPRINT "POCO/vayu_global/vayu:12/RKQ1.200826.002/V13.0.5.0.SJUMIXM:user/release-keys"
 
-static const variant_info_t vayu_global_info = {
-    .hwc_value = "GLOBAL",
-    .sku_value = "",
-
-    .brand = "POCO",
-    .device = "vayu",
-    .marketname = "POCO X3 Pro",
-    .model = "M2102J20SG",
-    .build_fingerprint = FINGERPRINT,
-
-    .nfc = NFC_TYPE_NFC,
-};
-
-static const variant_info_t vayu_eea_info = {
-    .hwc_value = "EEA",
-    .sku_value = "",
-
-    .brand = "POCO",
-    .device = "vayu",
-    .marketname = "POCO X3 Pro",
-    .model = "M2102J20SG",
-    .build_fingerprint = FINGERPRINT,
-
-    .nfc = NFC_TYPE_NFC,
-};
-
 static const variant_info_t bhima_info = {
     .hwc_value = "INDIA",
     .sku_value = "",
@@ -50,10 +24,22 @@ static const variant_info_t bhima_info = {
     .nfc = NFC_TYPE_NONE,
 };
 
+static const variant_info_t vayu_info = {
+    .hwc_value = "",
+    .sku_value = "",
+
+    .brand = "POCO",
+    .device = "vayu",
+    .marketname = "POCO X3 Pro",
+    .model = "M2102J20SG",
+    .build_fingerprint = FINGERPRINT,
+
+    .nfc = NFC_TYPE_NFC,
+};
+
 static const std::vector<variant_info_t> variants = {
-    vayu_global_info,
-    vayu_eea_info,
     bhima_info,
+    vayu_info,
 };
 
 void vendor_load_properties() {
