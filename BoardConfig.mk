434774bc8a923af612e2927fd299001ada6e1de6
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-08-22 14:17:13 +0200

    vayu: Include vayu config fragment
    Change-Id: Ia314e587499ed33da36c195ac45ddc5a99c6cea2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_vayu
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_vayu
 
 # Kernel
-TARGET_KERNEL_CONFIG := vendor/vayu_defconfig
+TARGET_KERNEL_CONFIG += vendor/xiaomi/vayu.config
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
